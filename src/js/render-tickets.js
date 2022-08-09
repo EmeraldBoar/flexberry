521a43b33ea7d412fc5e946209da37baf6123b55
EmeraldBoar <kozlovkv174@gmail.com>
2022-08-09 11:44:18 +0300

    feat: add render ticket

---
+++ src/js/render-tickets.js
@@ -0,0 +1,22 @@
+import { createTicket } from "./create-ticket.js";
+import { filtersTickets } from "./filter.js";
+import { getSelectedComparison } from "./sort.js";
+
+
+const renderTickets = (tickets) => {
+  const ticketListElement = document.querySelector('.tickets');
+  const fragment = document.createDocumentFragment();
+
+  tickets
+    .slice()
+    .sort(getSelectedComparison())
+    .filter(filtersTickets)
+    .slice(0, 5)
+    .forEach((ticket) => {
+      fragment.appendChild(createTicket(ticket));
+    });
+  ticketListElement.innerHTML = '';
+  ticketListElement.appendChild(fragment);
+}
+
+export { renderTickets };
