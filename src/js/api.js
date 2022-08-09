4b8eef7a40a087b040859041def1f52aa83c507a
EmeraldBoar <kozlovkv174@gmail.com>
2022-08-09 11:43:40 +0300

    feat: add api

--- src/js/api.js
+++ src/js/api.js
@@ -1,35 +1,56 @@
-const URL = 'http://localhost:3003/tickets';
-const COUNT_OF_REQUESTS = 5;
-const DELAY_TIME = 1000;
+const ID_URL = 'https://front-test.dev.aviasales.ru/search';
+const TICKETS_URL = 'https://front-test.dev.aviasales.ru/tickets?searchId=';
+const COUNT_OF_REQUESTS = 30;
 
-const delay = (ms) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-const request = async(url, count) => {
+const getSearchId = async(url, count) => {
   try {
     const response = await fetch(url);
-    if(response.headers.get('Content-Type') !== 'application/json; charset=utf-8') {
-      throw new Error(`Некорректный данные от сервера. Ожидается: application/json; charset=utf-8. Получены: ${response.headers.get('Content-Type')}`);
-    }
     if(response.ok) {
-      return response;
+      const id = await response.json();
+      return id.searchId;
     }
-    throw new Error('Ошбика fetch запроса');
+    throw new Error('Ошбика получения ID');
   } catch (error) {
     if (count <= 1) {
       throw error;
     }
-    await delay(DELAY_TIME);
-    return await request(url, count - 1);
+    return await getSearchId(url, count - 1);
   }
 };
 
+const getTickets = async () =>{
+  const searchId = await getSearchId(ID_URL, COUNT_OF_REQUESTS);
+  const requestUrl = `${TICKETS_URL}${searchId}`;
+  const tickets = [];
+
+  const request = async(url, count) => {
+    try {
+      const response = await fetch(url);
+      
+      if(response.ok) {
+        const pack = await response.json();
+        tickets.push(...pack.tickets);
+        return pack.stop ? tickets : await request(url, count - 1);
+      }
+
+      throw new Error('Ошибка запроса');
+
+    } catch (error) {
+      if (count <= 1) {
+        throw error;
+      }
+      return await request(url, count - 1);
+    }
+  };
+
+  return request(requestUrl, COUNT_OF_REQUESTS);
+}
+
+
 const getData = async (onSuccess, onError) => {
   try {
-    const response = await request(URL, COUNT_OF_REQUESTS); 
-    const json = await response.json();
-    onSuccess(json);
+    const response = await getTickets();
+    onSuccess(response);
 
   } catch(error) {
     onError(error);
