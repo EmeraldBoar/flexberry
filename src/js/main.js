5b8b63fdca7aa5718456aaff0270c97cb0d4ba56
EmeraldBoar <kozlovkv174@gmail.com>
2022-08-09 11:46:32 +0300

    feat: add inactive block state

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,34 @@
 import { getData } from './api.js';
+import { toggleStatePage } from './state-page.js';
+import { renderTickets } from './render-tickets.js';
+import { setSortClick } from './sort.js';
+import { setFilterCLick } from './filter.js';
+import { renderPlugs } from './render-plugs.js';
+import { debounce } from './util.js';
 
+const RERENDER_DELAY = 300;
+
+
+
+
+toggleStatePage(false);
+renderPlugs();
 getData(
   (tickets) => {
-    console.log(tickets);
+    toggleStatePage(true);
+    renderTickets(tickets);
+    setSortClick(debounce(
+      () => {
+        renderTickets(tickets)},
+      RERENDER_DELAY
+      ));
+    setFilterCLick(debounce(
+      () => renderTickets(tickets),
+      RERENDER_DELAY
+      ));
   },
   (error) => console.log(error),
 );
+
+
+
